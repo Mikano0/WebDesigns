ee9593d4a4d8b2a5e09dc65c7594424b832b31e9
Mikano <113972333+Mikano0@users.noreply.github.com>
2026-01-28 18:20:08 +0100

    Add files via upload

---
+++ BookList/main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import  DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+import os
+
+
+class Base(DeclarativeBase):
+    pass
+db = SQLAlchemy(model_class=Base)
+ 
+app = Flask(__name__)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{os.path.join(BASE_DIR, 'new-books-collection.db')}"
+db.init_app(app)
+
+class Book(db.Model):
+    id:Mapped[int] = mapped_column(Integer, primary_key=True)
+    title:Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    author:Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    rating:Mapped[float] = mapped_column(Float, nullable=False)
+
+with app.app_context():
+    db.create_all()
+
+
+
+@app.route('/')
+def home():
+    all_books = db.session.execute(db.select(Book).order_by(Book.title)).scalars().all()
+    return render_template("index.html", books = all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+        new_book = Book(title=request.form.get("book_name"), author=request.form.get("author"), rating=float(request.form.get("rating")))
+        db.session.add(new_book)
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("add.html")
+
+@app.route("/edit/<int:book_id>", methods=["GET", "POST"])
+def edit(book_id):
+    book = db.session.get(Book, book_id)
+    if request.method == "POST":
+        book.title = request.form.get("book_name")
+        book.author = request.form.get("author")
+        book.rating = request.form.get("rating")
+        db.session.commit()
+        return redirect(url_for("home"))
+    return render_template("edit.html", book=book)
+    
+@app.route("/delete/<int:book_id>", methods=["POST"])
+def delete(book_id):
+    book = db.session.get(Book, book_id)
+    db.session.delete(book)
+    db.session.commit()
+    return redirect(url_for("home"))
+if __name__ == "__main__":
+    app.run(debug=True)
+
