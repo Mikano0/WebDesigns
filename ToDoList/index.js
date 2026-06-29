8d680286393472ca2570e4ccdc9cf127d0368cf2
Mikano <113972333+Mikano0@users.noreply.github.com>
2026-06-29 13:17:11 +0200

    Refactor ToDoList to support multiple list types

--- ToDoList/index.js
+++ ToDoList/index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const db = new pg.Client({
   user: "postgres",
   host: "localhost",
-  database: "",
+  database: "todolist",
   password: "",
   port: 5432,
 });
@@ -18,41 +18,71 @@ db.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-let items = [
-  { id: 1, title: "Buy milk" },
-  { id: 2, title: "Finish homework" },
-];
+const routes = {
+  Today: "/",
+  Weekly: "/week",
+  Monthly: "/month",
+}
 
-async function getItems(){
-  const result = await db.query("SELECT * FROM list order by id ASC;")
+async function getItems(listType){
+  const result = await db.query("SELECT * FROM list WHERE list_type = $1 order by id ASC;",
+    [listType]
+  )
   let items = result.rows
   return items
 }
 
 app.get("/", async (req, res) => {
-  const items = await getItems()
+  const listType = "Today"
+
+  const items = await getItems(listType)
   console.log(items)
   res.render("index.ejs", {
-    listTitle: "Today",
+    listTitle: listType,
     listItems: items,
   });
 });
 
+app.get("/week", async(req,res) =>{
+  const listType = "Weekly"
+
+  const items = await getItems(listType)
+  console.log(items)
+  res.render("index.ejs",{
+    listTitle: listType,
+    listItems: items
+  })
+})
+
+app.get("/month", async(req,res) =>{
+  const listType = "Monthly"
+
+  const items = await getItems(listType)
+  console.log(items)
+  res.render("index.ejs",{
+    listTitle: listType,
+    listItems: items
+  })
+})
+
+
 app.post("/add", async (req, res) => {
+  console.log(req.body)
   const item = req.body.newItem;
-  console.log(item)
+  const listTitle = req.body.listTitle;
   try{
-    await db.query("INSERT INTO list(title) VALUES ($1);",
-      [item]
+    await db.query("INSERT INTO list(title, list_type) VALUES ($1, $2);",
+      [item, listTitle]
     )
   } catch(err){
     console.log(err)
   }
-  res.redirect("/");
+  res.redirect(routes[listTitle]);
 });
 
 app.post("/edit", async (req, res) => {
   console.log(req.body)
+  const listTitle = req.body.listTitle;
   const id = req.body.updatedItemId
   const title = req.body.updatedItemTitle
   try{
@@ -63,13 +93,14 @@ app.post("/edit", async (req, res) => {
     console.log(err)
   }
 
-  res.redirect("/");
+  res.redirect(routes[listTitle]);
 });
 
 
 app.post("/delete", async (req, res) => {
   console.log(req.body)
   const id = req.body.deleteItemId
+  const listTitle = req.body.listTitle;
   try{
     await db.query("DELETE from list where id = $1;",
       [id]
@@ -77,6 +108,7 @@ app.post("/delete", async (req, res) => {
   } catch (err){
     console.log(err)
   }
+  res.redirect(routes[listTitle])
 });
 
 app.listen(port, () => {
